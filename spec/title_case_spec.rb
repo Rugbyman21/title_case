78700ce1081bc29b631b628a6c9b666f20832664
Andrew Dang & Trevor Scerling <student@epicodus.com>
2015-08-03 10:37:12 -0700

    first spec: capitalize first letter in multile word title

--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -3,4 +3,7 @@ require('title_case')
 
 describe('String#title_case') do
 
+  it("capitalizes the first letter of all words in a multiple word title") do
+    expect(("the color purple").title_case()).to(eq("The Color Purple"))
+  end
 end
