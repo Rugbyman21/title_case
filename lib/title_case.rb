332dac6b91965c892b4c1b6690067e91fbaf7828
Andrew Dang & Trevor Scerling <student@epicodus.com>
2015-08-03 14:07:30 -0700

    add fourth spec and make it pass.

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,9 +1,22 @@
 class String
   define_method(:title_case) do
-    split_sentence = self.split
-    split_sentence.each do |word|
-      word.capitalize!()
+    final_case = []
+    ignored_words = ['from', 'to', 'the', 'on', 'in', 'and', 'for', 'a', 'or']
+    letter_position = 0
+    split_sentence = self.split()
+
+    split_sentence.each() do |word|
+      word.downcase!()
     end
     split_sentence.join(" ")
+
+    split_sentence.each() do |word|
+      if ignored_words.include?(word) && word.!=(split_sentence[0])
+        final_case.push(word)
+      else
+        final_case.push(word.capitalize!())
+      end
+    end
+    final_case.join(" ")
   end
 end
--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -4,6 +4,19 @@ require('title_case')
 describe('String#title_case') do
 
   it("capitalizes the first letter of all words in a multiple word title") do
+    expect(("color purple").title_case()).to(eq("Color Purple"))
+  end
+
+  it('does not capitalize designated words like prepositions, conjunctions, etc') do
+    expect(("beowulf from brighton beach").title_case()).to(eq("Beowulf from Brighton Beach"))
+  end
+
+  it("it capitalizes designated words if they are the first word") do
     expect(("the color purple").title_case()).to(eq("The Color Purple"))
   end
+
+  it('manages all uppercase entries and mixed case entries') do
+    expect(('BEOWULF ON THE ROCKS').title_case()).to(eq("Beowulf on the Rocks"))
+  end
+
 end
